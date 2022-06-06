3d50d64d14a072981d1ba6228ac711f39700009d
Tarek <tarek90.salim@live.com>
2022-06-06 18:16:09 +0200

    Finalized files

--- script.js
+++ script.js
@@ -1,14 +1,50 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
 let apiQuotes = [];
+
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
 
 // Show New Quote
 function newQuote() {
+    loading();
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    // Check if author field is blank and replace it with 'Unknown'
+    if (!quote.author) {
+        authorText.textContent = "Unknown";
+    } else {
+        authorText.textContent = quote.author;   
+    }
+
+    // Check quote lengh to determine the styling
+    if (quote.text.length > 150) {
+        quoteText.classList.add("long-quote");
+    } else {
+        quoteText.classList.remove("long-quote");
+    }
+    // Set Quote & Hide Loader
+    quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get Quotes from APl
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
@@ -19,10 +55,18 @@ async function getQuotes() {
     }
 }
 
+// Tweet a quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, "_blanck");
+}
+
+// Event Listener
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
 // On Load
 getQuotes();
-
-
 
 // // Get Quotes from Local API
 // function newQuote() {
